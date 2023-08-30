1aa1baa905e0f186b10f16fe0c3293d15c3ca92e
Seungwon Lee <seungwon.dev@gmail.com>
2023-08-30 20:45:51 +0900

    config: 린팅 적용 & 초기 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
@@ -10,33 +10,33 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
 
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "react/jsx-uses-vars": "error", // var로 JSX할당하면 에러
-    "react/button-has-type": "error", //button에 타입지정안하면 에러
-    "react/jsx-sort-props": [
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/jsx-uses-vars': 'error', // var로 JSX할당하면 에러
+    'react/button-has-type': 'error', //button에 타입지정안하면 에러
+    'react/jsx-sort-props': [
       // JSX prop을 정렬해줌
-      "warn",
+      'warn',
       {
         callbacksLast: true,
         shorthandFirst: true,
@@ -44,14 +44,14 @@ module.exports = {
         reservedFirst: true,
       },
     ],
-    "react/function-component-definition": [
-      "error",
+    'react/function-component-definition': [
+      'error',
       {
-        namedComponents: "function-declaration",
+        namedComponents: 'function-declaration',
       },
     ],
-    "react-hooks/exhaustive-deps": "warn",
-    "no-multiple-empty-lines": "error",
-    "no-console": ["error", { allow: ["warn", "error", "info"] }], // console.log() 금지
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-multiple-empty-lines': 'error',
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
   },
-};
+}
