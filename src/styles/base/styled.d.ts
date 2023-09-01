4ae71b7a23adae7e384667bb81d41f14ea0f4e6f
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-09-01 13:30:56 +0900

    refactor: FontSizeType 파스칼 케이스로 변경

--- src/styles/base/styled.d.ts
+++ src/styles/base/styled.d.ts
@@ -1,12 +1,12 @@
 import 'styled-components'
 import { FlexType } from '../constants/flex'
 import { ColorType } from '../constants/colors'
-import { fontSizeType } from './../constants/fontSize'
+import { FontSizeType } from './../constants/fontSize'
 
 declare module 'styled-components' {
   interface DefaultTheme extends ExtendedTheme {
     colors: ColorType
     flex: FlexType
-    fontSizes: fontSizeType
+    fontSizes: FontSizeType
   }
 }
--- src/styles/constants/fontSize.ts
+++ src/styles/constants/fontSize.ts
@@ -1,11 +1,11 @@
-export interface fontSizeType {
+export interface FontSizeType {
   default: string
   large: string
   medium: string
   small: string
 }
 
-export const fontSizes: fontSizeType = {
+export const fontSizes: FontSizeType = {
   default: '16px',
   large: '36px',
   medium: '20px',
